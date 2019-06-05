17be00bb51bad02450dd431ba1134879b8fc7ad6
james-braun <braun.james.robert@gmail.com>
2019-06-04 22:37:04 -0500

    next

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/full-stack-project-seven/precache-manifest.cbeb8e536eda3309deee2166241b7741.js"
+  "/full-stack-project-seven/precache-manifest.193ddd7a4fd51e648a35dbff1476057f.js"
 );
 
 self.addEventListener('message', (event) => {
